b09924c2520774da73fd73d8fcc55ee45c809652
pereirav <vanessa.vps29@gmail.com>
2020-02-05 15:12:53 +1300

    TBRJ-1 Adding book controller with mocked results

---
+++ server/api/controllers/book/book.router.js
@@ -0,0 +1,10 @@
+import * as express from 'express';
+import controller from './book.controller';
+
+export default express
+  .Router()
+  .post('/', controller.create)
+  .put('/:id', controller.update)
+  .get('/', controller.getAll)
+  .get('/:id', controller.get)
+  .delete('/:id', controller.delete);
--- server/routes.js
+++ server/routes.js
@@ -1,5 +1,7 @@
 import examplesRouter from './api/controllers/examples/router';
+import bookRouter from './api/controllers/book/book.router';
 
 export default function routes(app) {
   app.use('/tbrjar/examples', examplesRouter);
+  app.use('/tbrjar/book', bookRouter);
 }
