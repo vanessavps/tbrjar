e9d6c015c21bb30c727487672666f2bf5564001a
Vanessa da Silva <Vanessa.daSilva@anz.com>
2020-02-11 09:55:08 +1300

    Saving on file

--- server/api/services/book/book.db.service.js
+++ server/api/services/book/book.db.service.js
@@ -1,9 +1,24 @@
 import logger from '../../../common/logger';
+const createCsvWriter = require('csv-writer').createObjectCsvWriter;
+const csvWriter = createCsvWriter({
+    path: 'books.csv',
+    header: [
+        {id: 'title', title: 'title'},
+        {id: 'author', title: 'author'},
+        {id: 'numberOfPages', title: 'pages'}
+    ]
+});
+
 
 class BookDatabase {
     save(book)
     {
-        logger.info(`The book ${book.title} was saved`);
+        var records = [book];
+        csvWriter.writeRecords(records)
+        .then(()=> { 
+            logger.info(`The book ${book.title} was saved`);
+        });
+        
         return Promise.resolve(book);
     }
 }
