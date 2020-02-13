635531b14432d9f75e107edcb1edcbaaa2b22518
Vanessa da Silva <Vanessa.daSilva@anz.com>
2020-02-13 15:14:21 +1300

    Reading csv content and returning getAll

--- server/api/services/book/book.db.service.js
+++ server/api/services/book/book.db.service.js
@@ -1,42 +1,66 @@
+import fs from 'fs';
+import CsvReadableStream from 'csv-reader';
+import { createObjectCsvWriter } from 'csv-writer';
 import logger from '../../../common/logger';
-const fs = require('fs');
-const csvReadableStream = require('csv-reader');
+import Book from '../../model/book';
+// const createCsvWriter = csvWriter.createObjectCsvWriter;
 
-const createCsvWriter = require('csv-writer').createObjectCsvWriter;
-const csvWriter = createCsvWriter({
-    path: 'books.csv',
-    header: [
-        {id: 'title', title: 'title'},
-        {id: 'author', title: 'author'},
-        {id: 'numberOfPages', title: 'pages'}
-    ]
+const writer = createObjectCsvWriter({
+  path: 'books.csv',
+  header: [
+    { id: 'title', title: 'title' },
+    { id: 'author', title: 'author' },
+    { id: 'numberOfPages', title: 'pages' },
+  ],
 });
 
 
 class BookDatabase {
-    save(book) {
-        var records = [book];
-        csvWriter.writeRecords(records)
-        .then(()=> { 
-            logger.info(`The book ${book.title} was saved`);
-        });
-        
-        return Promise.resolve(book);
-    }
+  save(book) {
 
-    getAll(book) {
-        let inputStream = fs.createReadStream('books.csv', 'utf8');
-        inputStream
-        .pipe(new csvReadableStream({ parseNumbers: true, parseBooleans: true, trim: true , header: false}))
-        .on('data', function (row) {
-            console.log('A row arrived: ', row);
+    return new Promise(resolve => {
+    const records = [book];
+    writer.writeRecords(records)
+      .then(() => {
+       resolve(book);
+      });
+    });
+    
+
+   
+    //return Promise.resolve(book);
+  }
+
+  getAll() {
+    const inputStream = fs.createReadStream('books.csv', 'utf8');
+    const readableStream = new CsvReadableStream({
+      parseNumbers: true, parseBooleans: true, trim: true, skipHeader: true,
+    });
+    const books = [];
+
+    return new Promise(resolve => {
+      inputStream.pipe(readableStream)
+        .on('data', row => {
+          const book = new Book(row[0], row[1], row[2]);
+          books.push(book);
         })
-        .on('end', function (data) {
-            console.log('No more rows!');
+        .on('end', () => {
+          resolve(books);
         });
+    });
+  }
 
-        return Promise.resolve("books!");
-    }
+  get(id) {
+    return Promise.resolve(`This is get by id. Id ${id}`);
+  }
+
+  delete(id) {
+    return Promise.resolve(`The book ${id} was deleted`);
+  }
+
+  update(id) {
+    return Promise.resolve(`The book ${id} was updated`);
+  }
 }
 
 export default new BookDatabase();
--- server/api/services/book/book.service.js
+++ server/api/services/book/book.service.js
@@ -1,36 +1,31 @@
 import logger from '../../../common/logger';
 import db from './book.db.service';
-import Book from './../../model/book.js';
-
-var book = require('./../../model/book').default;
+import Book from '../../model/book';
 
 class BookService {
   getAll() {
     logger.info('Getting all');
-    return db.getAll(book);
+    return db.getAll();
   }
 
   get(id) {
     logger.info('Getting by id');
-    return Promise.resolve(`This is get by id. Id ${id}`);
+    return db.get(id);
   }
 
   save(data) {
-    logger.info('Service!');
-    logger.info(`This is the data ${data.title}`);
-
-    let book = new Book(data.title,data.author, data.numberOfPages);
-    logger.info(`This is the book ${book}`);
+    logger.debug(`Saving ${data.title}`); 
+    const book = new Book(data.title, data.author, data.numberOfPages);
 
     return db.save(book);
   }
 
   update(id) {
-    return Promise.resolve(`The book ${id} was updated`);
+    return db.update(id);
   }
 
   delete(id) {
-    return Promise.resolve(`The book ${id} was deleted`);
+    return db.delete(id);
   }
 }
 
